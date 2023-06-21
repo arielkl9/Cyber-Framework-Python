297be7f47b2f7493db556ccaeee17038a1dfb5e1
Ariel Klinerman <arielkl999a@gmail.com>
2023-06-21 13:31:37 -0400

    Update Prints.py

--- Prints.py
+++ Prints.py
@@ -3,11 +3,15 @@ import Misc
 class Prints:
     def menu():
         Misc.Misc.clear_screen()
-        print("Welcome To My Framework!!\n")
-        print("This Program Is Still In Development\n")
-        print("Please Choose Module:\n")
-        print("1. Recon Module\n")
-        print("2. Scan Module\n")
+        print("\n              Welcome To My Framework!!                 \n")
+        print("\n        This Program Is Still In Development            \n")
+        print("**********************************************************\n")
+        print("|                                                        |\n")
+        print("|    Modules:                                            |\n")
+        print("|    [1] Recon Module                                    |\n")
+        print("|    [2] Scan Module                                     |\n")
+        print("|                                                        |\n")
+        print("**********************************************************\n")
 
     def print_menu_recon():
         Misc.Misc.clear_screen()
@@ -35,10 +39,15 @@ class Prints:
         print("*******************************************************************\n")
 
     def print_socket_scan():
-        print("why people insist to go the hard way... anyways...\n")
-        print("What scan you want to use?\n")
-        print("1. Fast scan - scans only common ports\n")
-        print("2. Full scan - scans ALL ports (might take some time)\n")
+        print("*******************************************************************\n")
+        print("|                                                                 |\n")
+        print("|    What scan you want to use?                                   |\n")
+        print("|                                                                 |\n")
+        print("|    Options:                                                     |\n")
+        print("|    [1] Fast scan - scans only common ports                      |\n")
+        print("|    [2] Full scan - scans ALL ports (might take some time)       |\n")
+        print("|                                                                 |\n")
+        print("*******************************************************************\n")
 
     
     
