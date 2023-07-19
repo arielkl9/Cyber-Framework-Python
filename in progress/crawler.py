ecbfb15364c053a6202982e0098ee6acbc129d0b
Ariel Klinerman <arielkl999a@gmail.com>
2023-07-19 03:50:39 +0300

    workig

--- in progress/crawler.py
+++ in progress/crawler.py
@@ -12,10 +12,8 @@ class Crawler:
     def __init__(self) -> None:
         try:
             self.menu()
-            self.index = 0
-            self.threads = []
+            self.good_urls = []
             self.urls_found = []
-            self.url_crawled = []
             self.thread_counter = 0
             self.domain = self.get_domain()
             self.start_time = time.time()
@@ -31,63 +29,46 @@ class Crawler:
         return "END"
 
     def crawl(self, url):
-        if url not in self.url_crawled:
-            html_obj = self.get_url(url)
-            self.url_crawled.append(url)
-            if html_obj:
-                soup = BeautifulSoup(html_obj.text, "html.parser")
-                urls = soup.find_all("a")
-                if urls:
-                    threads = []
-                    with concurrent.futures.ThreadPoolExecutor(max_workers=9999999999) as executor:
-                        for a_tag in urls:
-                            try:
-                                self.thread_counter += 1
-                                threads.append(executor.submit(self.check_url(a_tag)))
-                            except KeyError:
-                                pass
-                    self.index += 1
-                    self.threads.append({f"Task {self.index}":[]})
-                    if threads:
-                        for i,thread in enumerate(threads):
-                            try:
-                                if str(thread._result) == "END":
-                                    self.threads[self.index - 1][f"Task {self.index}"].append(f"Thread No.{i+1} - END")
-                                elif str(thread._state) == 'FINISHED' and str(thread._result) != "END":
-                                    self.threads[self.index - 1][f"Task {self.index}"].append(f"Thread No.{i+1} - COMPLETE")                        
-                                else:
-                                    self.threads[self.index - 1][f"Task {self.index}"].append(f"Thread No.{i+1} - ERROR")
-                            except Exception:
-                                self.threads[self.index - 1][f"Task {self.index}"].append(f"Thread No.{i+1} - ERROR (Exception)")
-                                pass
+        if url not in self.urls_found:
+            self.urls_found.append(url)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=60) as main_executor:
+                html_obj = main_executor.submit(self.get_url,url)
+                self.thread_counter += 1
+            try:
+                if html_obj.result():
+                    soup = BeautifulSoup(html_obj.result().text, "html.parser")
+                    urls = soup.find_all("a")
+                    if urls:
+                        with concurrent.futures.ThreadPoolExecutor(max_workers=60) as executor:
+                            for a_tag in urls:
+                                if a_tag["href"] not in self.urls_found:
+                                    executor.submit(self.check_url,a_tag["href"])
+                                    self.thread_counter += 1
+            except Exception:
+                pass
         return self.__call__
     
-    def check_url(self,a_tag):
-        url = str(a_tag["href"])
-        if url:
-            if url[-1] != "/":
-                    url += "/"
-            if self.domain in url:
+    def check_url(self,url):
+        if url and url not in self.good_urls:
+            if self.domain in url and "http" in url:
                 tld_count = url.count(self.domain)
                 if self.base_domain in url and self.scan_type not in url and "#" not in url and tld_count == 1: # "?" not in url and -> only sites
-                    if url not in self.urls_found:
-                        self.urls_found.append(url)
-                        if url not in self.url_crawled:
-                            return self.crawl(url)
-            elif url[0] == "/" and "#" not in url and self.scan_type not in url:
-                if f"{self.url}{a_tag['href']}" not in self.urls_found:
-                    if self.get_url(f"{self.url}{a_tag['href']}"):
-                        self.urls_found.append(f"{self.url}{a_tag['href']}")
-                        if f"{self.url}/{a_tag['href']}" not in self.url_crawled:
-                            return self.crawl(f"{self.url}{a_tag['href']}")
-        return None
-
+                    self.good_urls.append(url)
+                    print(f"{len(self.good_urls)}: {url}\n")
+                    return self.crawl(url)
+            elif url[0] == "/" and "#" not in url and self.scan_type not in url and f"{self.url}{url}" not in self.good_urls:
+                if self.get_url(f"{self.url}{url}"):
+                    self.good_urls.append(f"{self.url}{url}")
+                    print(f"{len(self.good_urls)}: {self.url}{url}\n")
+                    return self.crawl(f"{self.url}{url}")
+            
+                            
     def get_url(self, url):
         try:
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
                 }
-            response = requests.get(url, headers, timeout=2)
+            response = requests.get(url, headers, timeout=6)
             if response.status_code == 200:
                 return response
             else:
@@ -97,31 +78,33 @@ class Crawler:
                 return None
 
     def set_url(self):
-            while True:
+        while True:
+            self.clear_screen()
+            input_url = input("Enter a URL: ")
+            if self.is_valid_url(input_url):
+                if input_url[-1] != "/":
+                    input_url += "/"
                 self.clear_screen()
-                input_url = input("Enter a URL: ")
-                if self.is_valid_url(input_url):
-                    if input_url[-1] != "/":
-                        input_url += "/"
-                    self.clear_screen()
-                    print(f"Crawling at: {input_url}\nTry to stay quiet")
-                    break
-                else:
-                    continue
-            return input_url
+                break
+            else:
+                continue
+        return input_url
 
     def time_end(self):
         end_time = time.time()
         elapsed_time = end_time - self.start_time
-        spm = len(self.url_crawled) / elapsed_time * 60
+        spm = len(self.good_urls) / elapsed_time * 60
         hours = int(elapsed_time // 3600)
         minutes = int((elapsed_time % 3600) // 60)
         seconds = int(elapsed_time % 60)
         formatted_time = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
         formatted_spm = "{:.2f}".format(spm)
-        print(f"Sites Per Minute: {formatted_spm}")
         print(f"Elapsed Time: {formatted_time}")
-
+        print(f"Sites Found: {len(self.urls_found)}")
+        print(f"Working Sites: {len(self.good_urls)}")
+        print(f"Threads Used: {self.thread_counter}")
+        print(f"Sites Per Minute: {formatted_spm}")
+        
     def get_domain(self):
         domain = self.url
         domain = domain.strip("https://")
@@ -138,11 +121,10 @@ class Crawler:
     def log(self):
         self.clear_screen()
         print("Ok Thats All I Got For Now\nYour Results Are Waiting For You In 'log.json' File\nGo Catch Them All!\nAnd Btw Here Some Stats If Youre Intrested :D")
-        print(f"Sites Found: {len(self.url_crawled)}")
         self.time_end()
         path = self.make_dir("Crawler")
         with open(f"{path}/log.json","w") as json_file:
-            json_file.writelines(json.dumps(self.url_crawled,indent=2))
+            json_file.writelines(json.dumps(self.good_urls,indent=2))
 
     def menu(self):     
         while True:
@@ -176,6 +158,7 @@ class Crawler:
                 self.scan_type = "#"
                 break   
         self.url = self.set_url()
+        print(f"Crawling at: {self.url}\nTry to stay quiet")
     
     def make_dir(self,folder):
         try:
