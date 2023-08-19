7dda13f6d64be4fdb051036303050eace6731a36
Ariel Klinerman <arielkl999a@gmail.com>
2023-08-19 19:26:38 +0300

    update

--- in progress/crawler.py
+++ in progress/crawler.py
@@ -87,7 +87,7 @@ class Crawler:
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
                 }
-            response = requests.get(url, headers, timeout=6)
+            response = requests.get(url, headers, timeout=12)
             if response.status_code == 200:
                 return response
             else:
